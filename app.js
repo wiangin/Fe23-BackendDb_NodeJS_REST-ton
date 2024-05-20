0606d332f44d555f7a2b157dab3e611acbe3b766
Ton <146097352+wiangin@users.noreply.github.com>
2024-05-20 17:20:59 +0200

    Comiit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 import ejs from "ejs";
 import db from "./module/db.js";
 import bodyParser from 'body-parser';
-import apiRouter from './module/router-app.js';
+import apiRouter from './module/router-api.js';
 import clientRouter from './module/router-client.js';
 
 const app = express();
@@ -18,37 +18,6 @@ app.use('/api', apiRouter);
 app.use('/', clientRouter);
 
 // app.use(bodyParser.json());
-
-
-//////// Reuest med endpoint * / * \\\\\\\\\\
-//////// Reuest med endpoint * / * \\\\\\\\\\
-//////// Reuest med endpoint * / * \\\\\\\\\\
-app.get('/', async (req, res) => {
-    const pageTitle =   "Grit Academy API";
-    const sql = 'SHOW TABLES';
-    const [dbData] = await db.query(sql);
-    console.log(dbData);
-    res.render('index',{dbData, pageTitle});
-});
-
-app.post('/', async (req, res) => {
-
-    const tableName = req.body;
-    console.log(tableName.userInput);
-    const pageTitle =   "Grit Academy API";
-
-    const sql = `SELECT * FROM ${tableName.userInput}`;
-    const [dbData] = await db.query(sql);
-    // console.log(dbData);
-
-    const sql2 = `DESCRIBE ${tableName.userInput}`;
-    const [dbDataHeaders] = await db.query(sql2);
-    // console.log(dbDataHeaders);
-    res.render('index',{dbData,pageTitle,dbDataHeaders});
-});
-
-
-
 
 //server configuration
 const port = 3000;
--- module/router-client.js
+++ module/router-client.js
@@ -3,6 +3,22 @@ import db from "./db.js";
 
 const clientRouter = Router();
 
+
+/////// END POINT */*\\\\\\
+/////// END POINT */*\\\\\\
+/////// END POINT */*\\\\\\
+clientRouter.get('/', async (req, res) => {
+    const pageTitle =   "Grit Academy API";
+    const sql = 'SHOW TABLES';
+    const [dbData] = await db.query(sql);
+    console.log(dbData);
+    res.render('index',{dbData, pageTitle});
+});
+
+
+/////// END POINT *studentTable*\\\\\\
+/////// END POINT *studentTable*\\\\\\
+/////// END POINT *studentTable*\\\\\\
 clientRouter.get('/studentTable', async (req,res) => {
     const pageTitle =   "Grit Academy API";
     const sql = 'SELECT * FROM students';
@@ -10,12 +26,32 @@ clientRouter.get('/studentTable', async (req,res) => {
     console.log(dbData);
 
     const sql2 = 'DESCRIBE students';
-    const [dbDataHeaders] = await db.query(sql2)
+    const [dbDataHeaders] = await db.query(sql2);
     res.render('studentTable',{dbData, pageTitle, dbDataHeaders});
 });
 
+clientRouter.post('/studentTable', async (req,res) => {
+    const pageTitle = "Grit Academy API";
+    const userInput = req.body;
+    console.log(userInput);
+
+    const insertQuery = `INSERT INTO students (fName, lName, town) VALUES ("${userInput.fName}","${userInput.lName}","${userInput.town}")`;
+    const addData = await db.query(insertQuery);
+
+    const sql = 'SELECT * FROM students';
+    const [dbData] = await db.query(sql);
+    console.log(dbData);
+
+    const sql2 = 'DESCRIBE students';
+    const [dbDataHeaders] = await db.query(sql2);
+    res.render('studentTable', {dbData, pageTitle, dbDataHeaders});
+});
+
+/////// END POINT *courseTable*\\\\\\
+/////// END POINT *courseTable*\\\\\\
+/////// END POINT *courseTable*\\\\\\
 clientRouter.get('/courseTable', async (req, res) => {
-    const pageTitle =   "Grit Academy API";
+    const pageTitle = "Grit Academy API";
     const sql = 'SELECT * FROM courses';
     const [dbData] = await db.query(sql);
     console.log(dbData);
@@ -23,7 +59,59 @@ clientRouter.get('/courseTable', async (req, res) => {
     const sql2 = 'DESCRIBE courses';
     const [dbDataHeaders] = await db.query(sql2);
     console.log(dbDataHeaders);
-    res.render('studentTable',{dbData, pageTitle, dbDataHeaders});
+    res.render('courseTable',{dbData, pageTitle, dbDataHeaders});
 })
+
+clientRouter.post('/courseTable', async (req,res) => {
+    const pageTitle = "Grit Academy API";
+    const userInput = req.body;
+    console.log(userInput);
+
+    const insertQuery = `INSERT INTO courses (name, description) VALUES ("${userInput.name}","${userInput.description}")`;
+    const addData = await db.query(insertQuery);
+
+    const sql = 'SELECT * FROM Courses';
+    const [dbData] = await db.query(sql);
+    console.log(dbData);
+
+    const sql2 = 'DESCRIBE courses';
+    const [dbDataHeaders] = await db.query(sql2);
+    res.render('courseTable', {dbData, pageTitle, dbDataHeaders});
+});
+
+/////// END POINT *studentsAndCourses*\\\\\\
+/////// END POINT *studentsAndCourses*\\\\\\
+/////// END POINT *studentsAndCourses*\\\\\\
+
+clientRouter.get('/studentsAndCourses', async (req, res) => {
+    const pageTitle = "Grit Academy API";
+    const sql = 'SELECT * FROM students_courses';
+    const [dbData] = await db.query(sql);
+    console.log(dbData);
+
+    const sql2 = 'DESCRIBE students_courses';
+    const [dbDataHeaders] = await db.query(sql2);
+    console.log(dbDataHeaders);
+    res.render('studentsAndCourses',{dbData, pageTitle, dbDataHeaders});
+});
+
+clientRouter.post('/studentsAndCourses', async (req,res) => {
+    const pageTitle = "Grit Academy API";
+    const userInput = req.body;
+    console.log(userInput);
+
+    const insertQuery = `INSERT INTO students_courses (students_id, courses_id) VALUES (${userInput.students_id}, ${userInput.courses_id})`;
+    const addData = await db.query(insertQuery);
+
+    const sql = 'SELECT * FROM students_courses';
+    const [dbData] = await db.query(sql);
+    console.log(dbData);
+
+    const sql2 = 'DESCRIBE students_courses';
+    const [dbDataHeaders] = await db.query(sql2);
+    res.render('studentsAndCourses', {dbData, pageTitle, dbDataHeaders});
+});
+
+
 
 export default clientRouter;
