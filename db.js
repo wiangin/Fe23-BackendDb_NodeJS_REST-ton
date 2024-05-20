006f7b6798446a0cb3905ae4ab53edf2b7d50729
Ton <146097352+wiangin@users.noreply.github.com>
2024-05-20 09:47:53 +0200

    Commit

---
+++ db.js
@@ -0,0 +1,16 @@
+import mysql from "mysql2/promise.js";
+
+// Create a pool of database connections
+const pool = mysql.createPool({
+    host: 'localhost',
+    port: '3308',
+    user: 'root',
+    password: '',
+    database: 'skoldatabas',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    rowsAsArray: true
+  });
+
+  export default pool;
